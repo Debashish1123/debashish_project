1aef9429cd43e890ecda029503e13b78819e5740
Nebsish <hinebasish@gmail.com>
2022-02-09 19:21:29 +0530

    initial commit

---
+++ spring-boot-crud-example/src/main/java/com/javatechie/crud/example/SpringBootCrudExample2Application.java
@@ -0,0 +1,18 @@
+package com.javatechie.crud.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Debasish Pradhan
+ *
+ */
+@SpringBootApplication
+public class SpringBootCrudExample2Application {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudExample2Application.class, args);
+	}
+
+}
